138900bdc5fd854458acbfa10c34ed16ce641454
jblanked <82678820+jblanked@users.noreply.github.com>
2025-07-28 14:02:16 -0400

    add dark mode

--- src/ArduinoIDE/Picoware/src/internal/gui/desktop.hpp
+++ src/ArduinoIDE/Picoware/src/internal/gui/desktop.hpp
@@ -12,9 +12,11 @@ namespace Picoware
         void clear();
         void draw(const uint8_t *animationFrame, Vector animationSize, const uint16_t *palette = nullptr);
         //
-        uint16_t getTextColor() const { return textColor; }
         uint16_t getBackgroundColor() const { return backgroundColor; }
+        uint16_t getTextColor() const { return textColor; }
         //
+        void setBackgroundColor(uint16_t color) { backgroundColor = color; }
+        void setTextColor(uint16_t color) { textColor = color; }
         void setTime(const char *time) { rtcTime = time; }
 
     private:
@@ -22,6 +24,7 @@ namespace Picoware
         Draw *display;
         uint16_t textColor;
         uint16_t backgroundColor;
+        bool darkMode;
         bool hasBattery;
         bool hasBluetooth;
         bool hasWiFi;

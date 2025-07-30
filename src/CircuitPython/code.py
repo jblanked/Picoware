5f2be9f76e7062fd35da7874a6147ef187abaa9c
jblanked <82678820+jblanked@users.noreply.github.com>
2025-07-30 16:24:08 -0400

    add all configs

--- src/CircuitPython/code.py
+++ src/CircuitPython/code.py
@@ -1,15 +1,22 @@
 """
-Picoware Game Engine
+Picoware
 Author: JBlanked
 Github: https://github.com/jblanked/Picoware
 Info: A custom firmware for the PicoCalc, Video Game Module, and other Raspberry Pi Pico devices.
 Created: 2025-05-14
-Updated: 2025-05-15
+Updated: 2025-07-30
 """
 
 from picoware.system.view import View
 from picoware.system.view_manager import ViewManager
-from picoware.system.boards import VGM_BOARD_CONFIG, JBLANKED_BOARD_CONFIG
+from picoware.system.boards import (
+    VGM_BOARD_CONFIG,
+    JBLANKED_BOARD_CONFIG,
+    PICO_CALC_BOARD_CONFIG_PICO,
+    PICO_CALC_BOARD_CONFIG_PICO_W,
+    PICO_CALC_BOARD_CONFIG_PICO_2,
+    PICO_CALC_BOARD_CONFIG_PICO_2W,
+)
 from picoware.applications.desktop import main as desktop
 from picoware.applications.loading import main as loading
 

3045b154265024328b84eee6d7c7b72a2336ffa0
jblanked <82678820+jblanked@users.noreply.github.com>
2025-08-08 08:52:19 -0400

    add color and button definitions

---
+++ src/SDK/Picoware/src/system/colors.hpp
@@ -0,0 +1,71 @@
+#pragma once
+
+// https://doc-tft-espi.readthedocs.io/tft_espi/colors/
+
+#ifndef TFT_WHITE
+#define TFT_WHITE 0xFFFF
+#endif
+
+#ifndef TFT_BLACK
+#define TFT_BLACK 0x0000
+#endif
+
+#ifndef TFT_BLUE
+#define TFT_BLUE 0x001F
+#endif
+
+#ifndef TFT_CYAN
+#define TFT_CYAN 0x07FF
+#endif
+
+#ifndef TFT_RED
+#define TFT_RED 0xF800
+#endif
+
+#ifndef TFT_LIGHTGREY
+#define TFT_LIGHTGREY 0xD69A
+#endif
+
+#ifndef TFT_DARKGREY
+#define TFT_DARKGREY 0x7BEF
+#endif
+
+#ifndef TFT_GREEN
+#define TFT_GREEN 0x07E0
+#endif
+
+#ifndef TFT_DARKCYAN
+#define TFT_DARKCYAN 0x03EF
+#endif
+
+#ifndef TFT_DARKGREEN
+#define TFT_DARKGREEN 0x03E0
+#endif
+
+#ifndef TFT_SKYBLUE
+#define TFT_SKYBLUE 0x867D
+#endif
+
+#ifndef TFT_VIOLET
+#define TFT_VIOLET 0x915C
+#endif
+
+#ifndef TFT_BROWN
+#define TFT_BROWN 0x9A60
+#endif
+
+#ifndef TFT_TRANSPARENT
+#define TFT_TRANSPARENT 0x0120
+#endif
+
+#ifndef TFT_YELLOW
+#define TFT_YELLOW 0xFFE0
+#endif
+
+#ifndef TFT_ORANGE
+#define TFT_ORANGE 0xFDA0
+#endif
+
+#ifndef TFT_PINK
+#define TFT_PINK 0xFE19
+#endif

b48882dbb56a000114de0a6dddd5dff0a6279014
jblanked <82678820+jblanked@users.noreply.github.com>
2025-08-07 21:05:08 -0400

    merge drivers from @BlairLeduc text starter
    https://github.com/BlairLeduc/picocalc-text-starter/tree/main

--- src/SDK/Picoware/Picoware.cpp
+++ src/SDK/Picoware/Picoware.cpp
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-
+#include "keyboard.h"
+#include "southbridge.h"
+#include "lcd.h"
 
 int main()
 {
     stdio_init_all();
+    sb_init();
+    //  keyboard_init(picocalc_chars_available_notify);
+    // fat32_init();
+    lcd_init();
 
-    while (true) {
+    while (true)
+    {
         printf("Hello, world!\n");
         sleep_ms(1000);
     }

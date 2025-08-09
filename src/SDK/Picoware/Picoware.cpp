7b297a8bbf036bd844d93e629429844908543a8d
jblanked <82678820+jblanked@users.noreply.github.com>
2025-08-08 21:58:35 -0400

    add pico w/pico 2 w support

--- src/SDK/Picoware/Picoware.cpp
+++ src/SDK/Picoware/Picoware.cpp
@@ -1,16 +1,26 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "src/system/drivers/keyboard.h"
 #include "src/system/drivers/southbridge.h"
 #include "src/system/drivers/lcd.h"
+
+#ifdef CYW43_WL_GPIO_LED_PIN
+// WiFi-specific code for Pico W or Pico 2W
+#include "pico/cyw43_arch.h"
+#endif
 
 int main()
 {
     stdio_init_all();
     sb_init();
-    //  keyboard_init(picocalc_chars_available_notify);
-    // fat32_init();
     lcd_init();
+
+#ifdef CYW43_WL_GPIO_LED_PIN
+    if (cyw43_arch_init())
+    {
+        printf("WiFi init failed\n");
+        return -1;
+    }
+#endif
 
     while (true)
     {

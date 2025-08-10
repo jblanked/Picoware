48624fc1a52d3c85921256a38dcd8714b72a20ff
jblanked <82678820+jblanked@users.noreply.github.com>
2025-08-10 19:06:04 -0400

    add Scrollbar gui class

---
+++ src/SDK/Picoware/src/gui/scrollbar.cpp
@@ -0,0 +1,91 @@
+#include "../gui/scrollbar.hpp"
+
+ScrollBar::ScrollBar(Draw *draw, Vector position, Vector size, uint16_t outlineColor, uint16_t fillColor)
+    : display(draw), position(position), size(size), outlineColor(outlineColor), fillColor(fillColor)
+{
+    this->draw();
+}
+
+ScrollBar::~ScrollBar()
+{
+    // nothing to do
+}
+
+void ScrollBar::clear()
+{
+    this->display->clear(position, size, fillColor);
+}
+
+void ScrollBar::draw()
+{
+    this->display->drawRect(position, size, outlineColor);
+    this->display->fillRect(Vector(position.x + 1, position.y + 1), Vector(size.x - 2, size.y - 2), fillColor);
+}
+
+void ScrollBar::setAll(Vector newPosition, Vector newSize, uint16_t newOutlineColor, uint16_t newFillColor, bool shouldDraw, bool shouldClear)
+{
+    if (shouldClear)
+    {
+        this->clear();
+    }
+    this->position = newPosition;
+    this->size = newSize;
+    this->outlineColor = newOutlineColor;
+    this->fillColor = newFillColor;
+    if (shouldDraw)
+    {
+        this->draw();
+    }
+}
+
+void ScrollBar::setPosition(Vector newPosition, bool shouldDraw, bool shouldClear)
+{
+    if (shouldClear)
+    {
+        this->clear();
+    }
+    this->position = newPosition;
+    if (shouldDraw)
+    {
+        this->draw();
+    }
+}
+
+void ScrollBar::setSize(Vector newSize, bool shouldDraw, bool shouldClear)
+{
+    if (shouldClear)
+    {
+        this->clear();
+    }
+    this->size = newSize;
+    if (shouldDraw)
+    {
+        this->draw();
+    }
+}
+
+void ScrollBar::setOutlineColor(uint16_t newColor, bool shouldDraw, bool shouldClear)
+{
+    if (shouldClear)
+    {
+        this->clear();
+    }
+    this->outlineColor = newColor;
+    if (shouldDraw)
+    {
+        this->draw();
+    }
+}
+
+void ScrollBar::setFillColor(uint16_t newColor, bool shouldDraw, bool shouldClear)
+{
+    if (shouldClear)
+    {
+        this->clear();
+    }
+    this->fillColor = newColor;
+    if (shouldDraw)
+    {
+        this->draw();
+    }
+}
---
+++ src/SDK/Picoware/src/gui/scrollbar.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include "../gui/draw.hpp"
+
+class ScrollBar
+{
+public:
+    ScrollBar(Draw *draw, Vector position, Vector size, uint16_t outlineColor = TFT_BLACK, uint16_t fillColor = TFT_WHITE);
+    ~ScrollBar();
+    //
+    void clear();
+    void draw();
+    //
+    void setAll(Vector newPosition, Vector newSize, uint16_t newOutlineColor, uint16_t newFillColor, bool shouldDraw = false, bool shouldClear = false);
+    void setPosition(Vector newPosition, bool shouldDraw = false, bool shouldClear = false);
+    void setSize(Vector newSize, bool shouldDraw = false, bool shouldClear = false);
+    void setOutlineColor(uint16_t newColor, bool shouldDraw = false, bool shouldClear = false);
+    void setFillColor(uint16_t newColor, bool shouldDraw = false, bool shouldClear = false);
+    //
+    Vector getPosition() const { return position; }
+    Vector getSize() const { return size; }
+    uint16_t getOutlineColor() const { return outlineColor; }
+    uint16_t getFillColor() const { return fillColor; }
+
+private:
+    Draw *display;
+    Vector position;
+    Vector size;
+    uint16_t outlineColor;
+    uint16_t fillColor;
+};

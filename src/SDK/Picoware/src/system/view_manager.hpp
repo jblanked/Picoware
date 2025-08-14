a7eab5415bf83f95b15b350715b94db3e1b6fe41
jblanked <82678820+jblanked@users.noreply.github.com>
2025-08-14 14:29:20 -0400

    create View and ViewManager classes

---
+++ src/SDK/Picoware/src/system/view_manager.hpp
@@ -0,0 +1,82 @@
+#pragma once
+#include "../gui/draw.hpp"
+#include "../system/colors.hpp"
+#include "../system/led.hpp"
+#include "../system/storage.hpp"
+#include "../system/input.hpp"
+#include "../gui/keyboard.hpp"
+#include "../system/view.hpp"
+#include "../system/wifi.hpp"
+#define DARK_MODE_LOCATION "/dark_mode.json"
+
+class ViewManager
+{
+public:
+    ViewManager();
+    ~ViewManager();
+    //
+    bool add(const View *view);
+    void back(bool removeCurrentView = true);
+    void clearStack();
+    void pushView(const char *viewName);
+    void remove(const char *viewName);
+    void run();
+    void set(const char *viewName);
+    void switchTo(const char *viewName, bool clearStack = false, bool push = true);
+    //
+    uint16_t getBackgroundColor() const noexcept { return backgroundColor; }
+    const View *getCurrentView() const noexcept { return currentView; }
+    Draw *getDraw() const noexcept { return draw; }
+    uint16_t getForegroundColor() const noexcept { return foregroundColor; }
+    Input *getInputManager() const noexcept { return inputManager; }
+    Keyboard *getKeyboard() const noexcept { return keyboard; }
+    LED getLED() const noexcept { return led; }
+    uint16_t getSelectedColor() const noexcept { return selectedColor; }
+    Vector getSize() const noexcept { return draw->getSize(); }
+    uint8_t getStackDepth() const noexcept { return stackDepth; }
+    Storage getStorage() const noexcept { return storage; }
+    const View *getView(const char *viewName) const noexcept;
+    WiFi getWiFi() const noexcept { return wifi; }
+    const char *getTime()
+    {
+        if (wifi.isConnected())
+        {
+            struct tm timeinfo;
+            if (wifi.setTime(timeinfo, 5000))
+            {
+                static char timeBuffer[9]; // Buffer for formatted time string (HH:MM:SS)
+                sprintf(timeBuffer, "%02d:%02d:%02d", timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
+                return timeBuffer;
+            }
+        }
+        return nullptr;
+    }
+
+    void setBackgroundColor(uint16_t color) noexcept { backgroundColor = color; }
+    void setForegroundColor(uint16_t color) noexcept { foregroundColor = color; }
+    void setSelectedColor(uint16_t color) noexcept { selectedColor = color; }
+
+private:
+    void clear();
+    Draw *draw;
+    Input *inputManager;
+    Keyboard *keyboard;
+    const View *currentView;
+    Storage storage;
+    uint8_t viewCount;
+    static constexpr size_t MAX_VIEWS = 10;
+    const View *views[MAX_VIEWS];
+    uint16_t backgroundColor;
+    uint16_t foregroundColor;
+    uint16_t selectedColor;
+    int delayTicks;
+    int delayElapsed;
+    LED led;
+    WiFi wifi;
+
+    // View stack for navigation history
+    void _pushView(const View *view);
+    static constexpr size_t MAX_STACK_SIZE = 10;
+    const View *viewStack[MAX_STACK_SIZE];
+    uint8_t stackDepth;
+};

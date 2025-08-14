9347ef5108533fa8618bd5e804735f9ea68ddbe7
jblanked <82678820+jblanked@users.noreply.github.com>
2025-08-14 16:11:27 -0400

    add helpers for starfeid/spiro

--- src/SDK/Picoware/src/system/helpers.hpp
+++ src/SDK/Picoware/src/system/helpers.hpp
@@ -2,11 +2,50 @@
 #include <cstdint>
 #include <cstdlib>
 #include <string>
+#include <cmath>
+#include <ctime>
 #include <algorithm>
 #include "pico/time.h"
+
 #define delay(ms) sleep_ms(ms)
 #define millis() (to_ms_since_boot(get_absolute_time()))
+#define micros() (to_us_since_boot(get_absolute_time()))
+
 using String = std::string;
+
+static inline int mapValue(int x, int in_min, int in_max, int out_min, int out_max)
+{
+    if (in_max == in_min)
+        return out_min;
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+static inline void seedRandomOnce()
+{
+    static bool seeded = false;
+    if (!seeded)
+    {
+        std::srand((unsigned)time(nullptr));
+        seeded = true;
+    }
+}
+
+static inline int randomMax(int maxVal)
+{
+    if (maxVal <= 0)
+        return 0;
+    seedRandomOnce();
+    return std::rand() % maxVal;
+}
+
+static inline int randomRange(int minVal, int maxVal)
+{
+    if (maxVal <= minVal)
+        return minVal;
+    seedRandomOnce();
+    return minVal + (std::rand() % (maxVal - minVal));
+}
+
 inline void trim(std::string &str)
 {
     // Remove leading whitespace

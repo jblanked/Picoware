286c7eb228121a35bbf3f0d8774d9d28f372e386
jblanked <82678820+jblanked@users.noreply.github.com>
2025-08-27 14:29:47 -0400

    overwrite file + add debug + improve createDir

--- src/SDK/Picoware/src/system/storage.cpp
+++ src/SDK/Picoware/src/system/storage.cpp
@@ -1,5 +1,6 @@
 #include "../system/storage.hpp"
 #include "../system/drivers/fat32.h"
+#include "stdio.h"
 
 Storage::Storage()
 {
@@ -19,6 +20,7 @@ bool Storage::createFile(const char *filePath)
     {
         if (fat32_create(&file, filePath) != FAT32_OK)
         {
+            printf("Failed to create file.\n");
             status = false;
         }
     }
@@ -29,13 +31,27 @@ bool Storage::createFile(const char *filePath)
 bool Storage::createDirectory(const char *dirPath)
 {
     fat32_file_t dir;
-    fat32_entry_t entry;
-    if (fat32_dir_read(&dir, &entry) != FAT32_OK)
+    if (fat32_open(&dir, dirPath) == FAT32_OK)
     {
-        if (fat32_dir_create(&dir, dirPath) != FAT32_OK)
+        // Directory already exists, check if it's actually a directory
+        if (dir.attributes & FAT32_ATTR_DIRECTORY)
         {
+            fat32_close(&dir);
+            return true;
+        }
+        else
+        {
+            fat32_close(&dir);
+            printf("Path exists but is not a directory.\n");
             return false;
         }
+    }
+
+    // Directory doesn't exist, try to create it
+    if (fat32_dir_create(&dir, dirPath) != FAT32_OK)
+    {
+        printf("Failed to create directory.\n");
+        return false;
     }
     fat32_close(&dir);
     return true;
@@ -46,6 +62,7 @@ uint32_t Storage::getFileSize(const char *filePath)
     fat32_file_t file;
     if (fat32_open(&file, filePath) != FAT32_OK)
     {
+        printf("Failed to open file for reading.\n");
         return 0;
     }
     uint32_t size = fat32_size(&file);
@@ -58,6 +75,7 @@ bool Storage::read(const char *filePath, void *buffer, size_t size)
     fat32_file_t file;
     if (fat32_open(&file, filePath) != FAT32_OK)
     {
+        printf("Failed to open file for reading.\n");
         return false;
     }
     size_t bytes_read;
@@ -73,6 +91,7 @@ bool Storage::read(const char *filePath, void *buffer, size_t size, size_t *byte
     {
         if (bytes_read)
             *bytes_read = 0;
+        printf("Failed to open file for reading.\n");
         return false;
     }
     const bool status = fat32_read(&file, buffer, size, bytes_read) == FAT32_OK;
@@ -85,6 +104,7 @@ bool Storage::remove(const char *filePath)
     fat32_file_t file;
     if (fat32_open(&file, filePath) != FAT32_OK)
     {
+        printf("Failed to open file for removal.\n");
         return true;
     }
     const bool status = fat32_delete(filePath) == FAT32_OK;
@@ -97,6 +117,7 @@ bool Storage::rename(const char *oldPath, const char *newPath)
     fat32_file_t file;
     if (fat32_open(&file, oldPath) != FAT32_OK)
     {
+        printf("Failed to open file for renaming.\n");
         return false;
     }
     const bool status = fat32_rename(oldPath, newPath) == FAT32_OK;
@@ -104,14 +125,36 @@ bool Storage::rename(const char *oldPath, const char *newPath)
     return status;
 }
 
-bool Storage::write(const char *filePath, const void *data, size_t size)
+bool Storage::write(const char *filePath, const void *data, size_t size, bool overwrite)
 {
     fat32_file_t file;
     if (fat32_open(&file, filePath) != FAT32_OK)
     {
-        if (fat32_create(&file, filePath) != FAT32_OK)
+        if (!createFile(filePath))
         {
+            printf("Failed to create file.\n");
             return false;
+        }
+    }
+    else
+    {
+        if (overwrite)
+        {
+            if (fat32_delete(filePath) != FAT32_OK)
+            {
+                printf("Failed to delete existing file.\n");
+                return false;
+            }
+            if (!createFile(filePath))
+            {
+                printf("Failed to create new file.\n");
+                return false;
+            }
+            if (fat32_open(&file, filePath) != FAT32_OK)
+            {
+                printf("Failed to open new file.\n");
+                return false;
+            }
         }
     }
     size_t bytes_written;
--- src/SDK/Picoware/src/system/storage.hpp
+++ src/SDK/Picoware/src/system/storage.hpp
@@ -12,6 +12,6 @@ public:
     bool read(const char *filePath, void *buffer, size_t size, size_t *bytes_read);
     bool remove(const char *filePath);
     bool rename(const char *oldPath, const char *newPath);
-    bool write(const char *filePath, const void *data, size_t size);
+    bool write(const char *filePath, const void *data, size_t size, bool overwrite = true);
     uint32_t getFileSize(const char *filePath);
 };

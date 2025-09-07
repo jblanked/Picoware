2f3d567fd32f87348e1b741df615efc1099b3919
jblanked <82678820+jblanked@users.noreply.github.com>
2025-09-07 01:05:04 -0400

    cleanup

--- src/MicroPython/main.py
+++ src/MicroPython/main.py
@@ -4,32 +4,22 @@ def main():
 
     # Initial cleanup
     gc.collect()
-    print(f"Initial memory: {gc.mem_free()}")
 
     from picoware.system.view_manager import ViewManager
     from picoware.system.view import View
     from picoware.applications import desktop
 
-    print(f"After imports memory: {gc.mem_free()}")
-
-    print("Starting ViewManager...")
 
     # Initialize the view manager
     vm: ViewManager = None
     try:
         vm: ViewManager = ViewManager()
 
-        print(f"After ViewManager init memory: {gc.mem_free()}")
-
         # Add views to the view manager
         vm.add(View("desktop_view", desktop.run, desktop.start, desktop.stop))
 
-        print(f"After adding views memory: {gc.mem_free()}")
-
         # Switch views
         vm.switch_to("desktop_view")
-
-        print(f"After switching views memory: {gc.mem_free()}")
 
         # Main loop
         while True:
@@ -49,7 +39,6 @@ def main():
     finally:
         # Final cleanup
         gc.collect()
-        print(f"Final memory: {gc.mem_free()}")
 
 
 # run the main function
--- src/MicroPython/picoware/applications/desktop.py
+++ src/MicroPython/picoware/applications/desktop.py
@@ -13,23 +13,31 @@ def start(view_manager) -> bool:
 
 def run(view_manager) -> None:
     """Animate the loading spinner."""
-    from picoware.system.buttons import BUTTON_BACK
+    from picoware.system.buttons import BUTTON_LEFT, BUTTON_CENTER, BUTTON_UP
+    from picoware.system.view import View
 
     global _desktop
     if _desktop:
         _desktop.draw_header()
         _desktop.display.swap()
 
-    button: int = view_manager.input_manager.get_last_button()
+    input_manager = view_manager.input_manager
+    button: int = input_manager.get_last_button()
 
-    if button == BUTTON_BACK:
-        from picoware.applications import system_info
-        from picoware.system.view import View
+    if button == BUTTON_LEFT:
+        from picoware.applications.system import system_info
 
+        input_manager.reset(True)
         view_manager.add(
             View("system_info", system_info.run, system_info.start, system_info.stop)
         )
         view_manager.switch_to("system_info")
+    elif button in (BUTTON_CENTER, BUTTON_UP):
+        from picoware.applications import library
+
+        input_manager.reset(True)
+        view_manager.add(View("library", library.run, library.start, library.stop))
+        view_manager.switch_to("library")
 
 
 def stop(view_manager) -> None:

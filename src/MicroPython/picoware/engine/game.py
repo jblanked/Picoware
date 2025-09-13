1bbce3294c9320d55a4b27b417664d8c3eee62b7
jblanked <82678820+jblanked@users.noreply.github.com>
2025-09-13 15:24:19 -0400

    add game engine

---
+++ src/MicroPython/picoware/engine/game.py
@@ -0,0 +1,121 @@
+from picoware.system.vector import Vector
+
+
+class Game:
+    """
+    Represents a game.
+    """
+
+    def __init__(
+        self,
+        name: str,
+        size: Vector,
+        draw,
+        input_manager,
+        foreground_color: int = 0xFFFF,
+        background_color: int = 0x0000,
+        perspective: int = 0,
+        start=None,
+        stop=None,
+    ):
+        """
+        Initializes the game.
+        :param name: str - the name of the game
+        :param size: Vector - the size of the game
+        :param draw: Draw - the draw object used to render the game
+        :param input_manager: InputManager - the input manager used to handle input
+        :param foreground_color: int - the foreground color of the game
+        :param background_color: int - the background color of the game
+        :param perspective: int - the camera perspective (first person or third person)
+        :param start: function(Game) - the function called when the game is started
+        :param stop: function(Game) - the function called when the game is stopped
+        """
+        self.name = name
+        self._start = start
+        self._stop = stop
+        self.levels = []  # List of levels in the game
+        self.current_level = None  # holds the current level
+        self.input_manager = input_manager
+        self.input: int = -1  # last button pressed
+        self.draw = draw
+        self.camera = Vector(0, 0)
+        self.position = Vector(0, 0)
+        self.size = size
+        self.world_size = size
+        self.is_active = False
+        self.foreground_color = foreground_color
+        self.background_color = background_color
+        self.camera_perspective = perspective  # first person or third person
+
+    def __del__(self):
+        self.stop()
+
+    def clamp(self, value, lower, upper):
+        """Clamp a value between a lower and upper bound."""
+        return min(max(value, lower), upper)
+
+    def level_add(self, level):
+        """Add a level to the game"""
+        self.levels.append(level)
+
+    def level_remove(self, level):
+        """Remove a level from the game"""
+        self.levels.remove(level)
+
+    def level_switch(self, level):
+        """Switch to a new level"""
+        if not level:
+            print("Level is not valid.")
+            return
+        if self.current_level:
+            self.current_level.stop()
+
+        self.current_level = level
+        self.current_level.start()
+
+    def render(self):
+        """Render the current level"""
+        if self.current_level:
+            self.current_level.render()
+
+    def start(self) -> bool:
+        """Start the game"""
+        if not self.levels:
+            print("The game has no levels.")
+            return False
+        self.current_level = self.levels[0]
+        if self._start:
+            self._start(self)
+        self.current_level.start()
+        self.is_active = True
+        return True
+
+    def stop(self):
+        """Stop the game"""
+
+        if not self.is_active:
+            return
+
+        if self._stop:
+            self._stop(self)
+
+        self.is_active = False
+
+        for level in self.levels:
+            if level:
+                level.clear()
+                level = None
+        self.levels = []
+
+        self.draw.clear(Vector(0, 0), self.size, self.background_color)
+
+    def update(self):
+        """Update the game input and entity positions in a thread-safe manner."""
+        if not self.is_active:
+            return
+
+        self.input = self.input_manager.get_last_button()
+
+        # update the level
+        if self.current_level:
+            self.current_level.update()
---
+++ src/MicroPython/picoware/engine/level.py
@@ -0,0 +1,132 @@
+from micropython import const
+from picoware.system.vector import Vector
+
+CAMERA_FIRST_PERSON = const(0)
+CAMERA_THIRD_PERSON = const(1)
+
+
+class Level:
+    """
+    Represents a level in the game.
+    """
+
+    def __init__(
+        self,
+        name: str,
+        size: Vector,
+        game,
+        start=None,  # start is a function that is called when the level is created
+        stop=None,  # stop is a function that is called when the level is destroyed
+    ):
+        """
+        Initializes the level.
+
+        :param name: str - the name of the level
+        :param size: Vector - the size of the level
+        :param game: Game - the game to which the level belongs
+        :param start: function(Level) - the function called when the level is created
+        :param stop: function(Level) - the function called when the level is destroyed
+        """
+        self.name = name
+        self.size = size
+        self.game = game
+        self.entities = []  # List of entities in the level
+        self._clear_allowed: bool = True
+        self._start = start
+        self._stop = stop
+
+    @property
+    def clear_allowed(self) -> bool:
+        """Get if the level is allowed to clear the screen"""
+        return self._clear_allowed
+
+    @clear_allowed.setter
+    def clear_allowed(self, value: bool):
+        """Set if the level is allowed to clear the screen"""
+        self._clear_allowed = value
+
+    def clear(self):
+        """Clear the level"""
+        for entity in self.entities:
+            entity.stop(self.game)
+        self.entities.clear()
+
+    def collision_list(self, entity) -> list:
+        """Return a list of entities that the entity collided with"""
+        collided = []
+        for other in self.entities:
+            if entity != other and self.is_collision(entity, other):
+                collided.append(other)
+        return collided
+
+    def entity_add(self, entity):
+        """Add an entity to the level"""
+        self.entities.append(entity)
+        entity.start(self.game)
+        entity.is_active = True
+
+    def entity_remove(self, entity):
+        """Remove an entity from the level"""
+        self.entities.remove(entity)
+
+    def has_collided(self, entity) -> bool:
+        """Check for collisions with other entities"""
+        for other in self.entities:
+            if entity != other and self.is_collision(entity, other):
+                return True
+        return False
+
+    def is_collision(self, entity, other) -> bool:
+        """Check if two entities collided using AABB logic"""
+        return (
+            entity.position.x < other.position.x + other.size.x
+            and entity.position.x + entity.size.x > other.position.x
+            and entity.position.y < other.position.y + other.size.y
+            and entity.position.y + entity.size.y > other.position.y
+        )
+
+    def render(self):
+        """Render the level"""
+        if self._clear_allowed:
+            self.game.draw.clear(
+                Vector(0, 0), self.game.size, self.game.background_color
+            )
+
+        for entity in self.entities:
+            if entity.is_active:
+                entity.render(self.game.draw, self.game)
+
+                if not entity.is_visible:
+                    continue
+
+                if entity.sprite:
+                    self.game.draw.image_bytearray(
+                        Vector(
+                            entity.position.x - self.game.position.x,
+                            entity.position.y - self.game.position.y,
+                        ),
+                        entity.size,
+                        entity.sprite._raw,
+                    )
+
+        if self._clear_allowed:
+            self.game.draw.swap()
+
+    def start(self):
+        """Start the level"""
+        if self._start:
+            self._start(self)
+
+    def stop(self):
+        """Stop the level"""
+        if self._stop:
+            self._stop(self)
+
+    def update(self):
+        """Update the level"""
+        for entity in self.entities:
+            if entity.is_active:
+                entity.update(self.game)
+                collided = self.collision_list(entity)
+                for other in collided:
+                    entity.collision(other, self.game)

0525ff0997d50cb0f492790f04483c22d90086db
jblanked <82678820+jblanked@users.noreply.github.com>
2025-11-27 22:00:28 -0500

    start Bluetooth class

---
+++ src/MicroPython/picoware/system/bluetooth.py
@@ -0,0 +1,19 @@
+class Bluetooth:
+    """A class to manage Bluetooth functionality."""
+
+    def __init__(self):
+        from bluetooth import BLE
+
+        self._ble = BLE()
+        self._ble.active(True)
+        self._ble.irq(self.__irq)
+
+    def __del__(self):
+        """Clean up Bluetooth resources."""
+        self._ble.active(False)
+        del self._ble
+        self._ble = None
+
+    def __irq(self, event, data):
+        """Handle Bluetooth IRQ events."""
+        pass

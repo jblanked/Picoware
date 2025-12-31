58dc5f7c8deb27d648752ec37786133e3958b927
jblanked <82678820+jblanked@users.noreply.github.com>
2025-12-30 19:11:48 -0500

    correct board name

--- src/MicroPython/boards/WAVESHARE_RP2350_TOUCH_LCD_3_49/mpconfigboard.cmake
+++ src/MicroPython/boards/WAVESHARE_RP2350_TOUCH_LCD_3_49/mpconfigboard.cmake
@@ -1,4 +1,4 @@
-# cmake file for Waveshare RP2350 Touch LCD 1.28
+# cmake file for Waveshare RP2350 Touch LCD 3.49 board
 set(PICO_BOARD "waveshare_rp2350_touch_lcd_3.49")
 set(PICO_PLATFORM "rp2350")
 set(PICO_NUM_GPIOS 48)

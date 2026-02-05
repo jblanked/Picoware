2e9e3fb45f7b24671c7bed947acc755d274b15fa
jblanked <82678820+jblanked@users.noreply.github.com>
2026-02-05 15:30:50 -0500

    click on network to save
    first we scan, then we populate the Menu with our results, then if a user selects a network, we save the SSID and send the user to the password view to save the password

--- src/MicroPython/picoware/applications/wifi/scan.py
+++ src/MicroPython/picoware/applications/wifi/scan.py
@@ -1,4 +1,97 @@
+from picoware.system.buttons import (
+    BUTTON_BACK,
+    BUTTON_UP,
+    BUTTON_DOWN,
+    BUTTON_LEFT,
+    BUTTON_RIGHT,
+    BUTTON_CENTER,
+)
+
 _scan = None
+
+
+def __save_ssid(view_manager) -> bool:
+    """Save the selected SSID"""
+    global _scan
+
+    if _scan is None:
+        return False
+
+    selected_ssid: str = _scan.current_item
+    ssid_name = selected_ssid.split(" (")[0]
+
+    from picoware.applications.wifi.utils import save_wifi_ssid
+
+    if not save_wifi_ssid(view_manager.storage, ssid_name):
+        # try one last time
+        return save_wifi_ssid(view_manager.storage, ssid_name)
+    return True
+
+
+def __should_save_choice(view_manager) -> bool:
+    """Choose whether to save the selected SSID"""
+    from picoware.gui.choice import Choice
+    from picoware.system.vector import Vector
+
+    choice = Choice(
+        view_manager.draw,
+        Vector(0, 0),
+        view_manager.draw.size,
+        f"Save SSID '{_scan.current_item.split(' (')[0]}'?",
+        ["No", "Yes"],
+        0,
+        view_manager.foreground_color,
+        view_manager.background_color,
+    )
+    choice.draw()
+    choice.open()
+
+    input_manager = view_manager.input_manager
+
+    while True:
+        _button = input_manager.button
+        if _button in (BUTTON_LEFT, BUTTON_UP):
+            input_manager.reset()
+            choice.scroll_up()
+        elif _button in (BUTTON_RIGHT, BUTTON_DOWN):
+            input_manager.reset()
+            choice.scroll_down()
+        elif _button == BUTTON_CENTER:
+            input_manager.reset()
+            if choice.is_open():
+                choice.close()
+
+            _state = choice.state
+            del choice
+            choice = None
+            if _state == 1:
+                return True
+            view_manager.draw.clear()
+            _scan.draw()
+            return False
+        elif _button == BUTTON_BACK:
+            input_manager.reset()
+            del choice
+            choice = None
+            view_manager.draw.clear()
+            _scan.draw()
+            return False
+
+
+def __switch_to_password_view(view_manager) -> None:
+    """Switch to password view"""
+    from picoware.applications.wifi import password
+    from picoware.system.view import View
+
+    view_manager.add(
+        View(
+            "wifi_password",
+            password.run,
+            password.start,
+            password.stop,
+        )
+    )
+    view_manager.switch_to("wifi_password")
 
 
 def start(view_manager) -> bool:
@@ -6,6 +99,7 @@ def start(view_manager) -> bool:
     from picoware.gui.menu import Menu
 
     global _scan
+
     if _scan is None:
         wifi = view_manager.wifi
 
@@ -26,11 +120,15 @@ def start(view_manager) -> bool:
             2,
         )
 
+        ssids: list[str] = []
+
         for ssid, bssid, channel, rssi, authmode, hidden in results:
             _ssid = ssid.decode("utf-8")
             if len(_ssid) == 0:
                 _ssid = "<hidden>"
-            _scan.add_item(f"{_ssid} ({rssi}dB)")
+            if _ssid not in ssids:
+                ssids.append(_ssid)
+                _scan.add_item(f"{_ssid} ({rssi}dB)")
 
         _scan.set_selected(0)
 
@@ -40,15 +138,6 @@ def start(view_manager) -> bool:
 
 def run(view_manager) -> None:
     """Run the app"""
-    from picoware.system.buttons import (
-        BUTTON_BACK,
-        BUTTON_UP,
-        BUTTON_DOWN,
-        BUTTON_LEFT,
-        BUTTON_RIGHT,
-    )
-
-    global _scan
     if not _scan:
         return
 
@@ -64,6 +153,13 @@ def run(view_manager) -> None:
     elif button == BUTTON_BACK:
         input_manager.reset()
         view_manager.back()
+    elif button == BUTTON_CENTER:
+        input_manager.reset()
+        if __should_save_choice(view_manager):
+            if not __save_ssid(view_manager):
+                view_manager.alert("Failed to save SSID!", True)
+                return
+            __switch_to_password_view(view_manager)
 
 
 def stop(view_manager) -> None:

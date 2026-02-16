b448c2c5e65fe47c23271d0519cad0f1fd14ddad
jblanked <82678820+jblanked@users.noreply.github.com>
2026-02-15 21:28:11 -0500

    track deletion
    prevents a double free that caused a freeze

--- src/MicroPython/auto_complete/auto_complete_mp.c
+++ src/MicroPython/auto_complete/auto_complete_mp.c
@@ -17,6 +17,7 @@ typedef struct
 {
     mp_obj_base_t base;
     AutoComplete context;
+    bool freed;
 } auto_complete_mp_obj_t;
 
 const mp_obj_type_t auto_complete_mp_type;
@@ -26,18 +27,25 @@ STATIC void auto_complete_mp_print(const mp_print_t *print, mp_obj_t self_in, mp
     (void)kind;
     auto_complete_mp_obj_t *self = MP_OBJ_TO_PTR(self_in);
     mp_print_str(print, "AutoComplete(");
-    mp_print_str(print, "suggestion_count=");
-    mp_obj_print_helper(print, mp_obj_new_int(self->context.suggestion_count), PRINT_REPR);
-    mp_print_str(print, ", suggestions=[");
-    for (uint8_t i = 0; i < self->context.suggestion_count; i++)
+    if (self->freed)
     {
-        if (i > 0)
-        {
-            mp_print_str(print, ", ");
-        }
-        mp_obj_print_helper(print, mp_obj_new_str(self->context.suggestions[i], strlen(self->context.suggestions[i])), PRINT_REPR);
+        mp_print_str(print, "freed=True");
     }
-    mp_print_str(print, "]");
+    else
+    {
+        mp_print_str(print, "suggestion_count=");
+        mp_obj_print_helper(print, mp_obj_new_int(self->context.suggestion_count), PRINT_REPR);
+        mp_print_str(print, ", suggestions=[");
+        for (uint8_t i = 0; i < self->context.suggestion_count; i++)
+        {
+            if (i > 0)
+            {
+                mp_print_str(print, ", ");
+            }
+            mp_obj_print_helper(print, mp_obj_new_str(self->context.suggestions[i], strlen(self->context.suggestions[i])), PRINT_REPR);
+        }
+        mp_print_str(print, "]");
+    }
     mp_print_str(print, ")");
 }
 
@@ -45,6 +53,7 @@ STATIC mp_obj_t auto_complete_mp_make_new(const mp_obj_type_t *type, size_t n_ar
 {
     auto_complete_mp_obj_t *self = mp_obj_malloc_with_finaliser(auto_complete_mp_obj_t, &auto_complete_mp_type);
     self->base.type = &auto_complete_mp_type;
+    self->freed = false;
     // Initialize AutoComplete structure
     if (!auto_complete_init(&self->context))
     {
@@ -56,7 +65,11 @@ STATIC mp_obj_t auto_complete_mp_make_new(const mp_obj_type_t *type, size_t n_ar
 STATIC mp_obj_t auto_complete_mp_del(mp_obj_t self_in)
 {
     auto_complete_mp_obj_t *self = MP_OBJ_TO_PTR(self_in);
-    auto_complete_free(&self->context);
+    if (!self->freed)
+    {
+        auto_complete_free(&self->context);
+        self->freed = true;
+    }
     return mp_const_none;
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(auto_complete_mp_del_obj, auto_complete_mp_del);
@@ -67,7 +80,7 @@ STATIC void auto_complete_mp_attr(mp_obj_t self_in, qstr attribute, mp_obj_t *de
     if (destination[0] == MP_OBJ_NULL)
     {
         // Load attributes
-        if (attribute == MP_QSTR_context)
+        if (attribute == MP_QSTR_context && !self->freed)
         {
             mp_obj_t tuple[2]; // suggestions, suggestion_count
             mp_obj_t suggestions_tuple[self->context.suggestion_count];
@@ -88,23 +101,22 @@ STATIC void auto_complete_mp_attr(mp_obj_t self_in, qstr attribute, mp_obj_t *de
 STATIC mp_obj_t auto_complete_mp_add_word(mp_obj_t self_in, mp_obj_t word_obj)
 {
     auto_complete_mp_obj_t *self = MP_OBJ_TO_PTR(self_in);
-    const char *word = mp_obj_str_get_str(word_obj);
-    return auto_complete_add_word(&self->context, word) ? mp_const_true : mp_const_false;
+    if (!self->freed)
+    {
+        const char *word = mp_obj_str_get_str(word_obj);
+        return auto_complete_add_word(&self->context, word) ? mp_const_true : mp_const_false;
+    }
+    return mp_const_false;
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_2(auto_complete_mp_add_word_obj, auto_complete_mp_add_word);
-
-STATIC mp_obj_t auto_complete_mp_free(mp_obj_t self_in)
-{
-    auto_complete_mp_obj_t *self = MP_OBJ_TO_PTR(self_in);
-    auto_complete_free(&self->context);
-    return mp_const_none;
-}
-STATIC MP_DEFINE_CONST_FUN_OBJ_1(auto_complete_mp_free_obj, auto_complete_mp_free);
 
 STATIC mp_obj_t auto_complete_mp_remove_suggestions(mp_obj_t self_in)
 {
     auto_complete_mp_obj_t *self = MP_OBJ_TO_PTR(self_in);
-    auto_complete_remove_suggestions(&self->context);
+    if (!self->freed)
+    {
+        auto_complete_remove_suggestions(&self->context);
+    }
     return mp_const_none;
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(auto_complete_mp_remove_suggestions_obj, auto_complete_mp_remove_suggestions);
@@ -112,7 +124,10 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_1(auto_complete_mp_remove_suggestions_obj, auto_c
 STATIC mp_obj_t auto_complete_mp_remove_words(mp_obj_t self_in)
 {
     auto_complete_mp_obj_t *self = MP_OBJ_TO_PTR(self_in);
-    auto_complete_remove_words(&self->context);
+    if (!self->freed)
+    {
+        auto_complete_remove_words(&self->context);
+    }
     return mp_const_none;
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(auto_complete_mp_remove_words_obj, auto_complete_mp_remove_words);
@@ -121,7 +136,7 @@ STATIC mp_obj_t auto_complete_mp_search(mp_obj_t self_in, mp_obj_t prefix_obj)
 {
     auto_complete_mp_obj_t *self = MP_OBJ_TO_PTR(self_in);
     const char *prefix = mp_obj_str_get_str(prefix_obj);
-    if (auto_complete_search(&self->context, prefix))
+    if (!self->freed && auto_complete_search(&self->context, prefix))
     {
         mp_obj_t tuple[self->context.suggestion_count];
         for (uint8_t i = 0; i < self->context.suggestion_count; i++)
@@ -142,7 +157,6 @@ STATIC const mp_rom_map_elem_t auto_complete_mp_locals_dict_table[] = {
     {MP_ROM_QSTR(MP_QSTR_search), MP_ROM_PTR(&auto_complete_mp_search_obj)},
     {MP_ROM_QSTR(MP_QSTR_remove_suggestions), MP_ROM_PTR(&auto_complete_mp_remove_suggestions_obj)},
     {MP_ROM_QSTR(MP_QSTR_remove_words), MP_ROM_PTR(&auto_complete_mp_remove_words_obj)},
-    {MP_ROM_QSTR(MP_QSTR_free), MP_ROM_PTR(&auto_complete_mp_free_obj)},
 };
 STATIC MP_DEFINE_CONST_DICT(auto_complete_mp_locals_dict, auto_complete_mp_locals_dict_table);
 

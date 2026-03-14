3ef8532a513035b956db33598542e80563a0cd6e
jblanked <82678820+jblanked@users.noreply.github.com>
2026-03-14 09:17:23 -0400

    use new `TextEditor` gui

--- builds/MicroPython/apps_unfrozen/Text Editor.py
+++ builds/MicroPython/apps_unfrozen/Text Editor.py
@@ -6,18 +6,13 @@ _keyboard_just_started = False
 
 def __start_text_editor(view_manager) -> None:
     """Start the text editor app"""
-    from picoware.gui.textbox import TextBox
+    from picoware.gui.text_editor import TextEditor
 
     global _textbox, _text_editor_started, _filename
 
     if _textbox is None:
-        draw = view_manager.draw
-        _textbox = TextBox(
-            draw,
-            0,
-            draw.size.y,
-            view_manager.foreground_color,
-            view_manager.background_color,
+        _textbox = TextEditor(
+            view_manager,
         )
 
         storage = view_manager.storage
@@ -25,104 +20,6 @@ def __start_text_editor(view_manager) -> None:
             _textbox.set_text(storage.read(_filename))
 
     _text_editor_started = True
-
-
-def __process_text_input(button: int, shift: bool) -> None:
-    """Process text input and update the textbox"""
-    from picoware.system import buttons
-
-    global _textbox
-
-    button_pressed = False
-
-    character_map = {
-        buttons.BUTTON_A: "a",
-        buttons.BUTTON_B: "b",
-        buttons.BUTTON_C: "c",
-        buttons.BUTTON_D: "d",
-        buttons.BUTTON_E: "e",
-        buttons.BUTTON_F: "f",
-        buttons.BUTTON_G: "g",
-        buttons.BUTTON_H: "h",
-        buttons.BUTTON_I: "i",
-        buttons.BUTTON_J: "j",
-        buttons.BUTTON_K: "k",
-        buttons.BUTTON_L: "l",
-        buttons.BUTTON_M: "m",
-        buttons.BUTTON_N: "n",
-        buttons.BUTTON_O: "o",
-        buttons.BUTTON_P: "p",
-        buttons.BUTTON_Q: "q",
-        buttons.BUTTON_R: "r",
-        buttons.BUTTON_S: "s",
-        buttons.BUTTON_T: "t",
-        buttons.BUTTON_U: "u",
-        buttons.BUTTON_V: "v",
-        buttons.BUTTON_W: "w",
-        buttons.BUTTON_X: "x",
-        buttons.BUTTON_Y: "y",
-        buttons.BUTTON_Z: "z",
-        #
-        buttons.BUTTON_0: "0",
-        buttons.BUTTON_1: "1",
-        buttons.BUTTON_2: "2",
-        buttons.BUTTON_3: "3",
-        buttons.BUTTON_4: "4",
-        buttons.BUTTON_5: "5",
-        buttons.BUTTON_6: "6",
-        buttons.BUTTON_7: "7",
-        buttons.BUTTON_8: "8",
-        buttons.BUTTON_9: "9",
-        #
-        buttons.BUTTON_CENTER: "\n",
-        buttons.BUTTON_SPACE: " ",
-        buttons.BUTTON_PERIOD: ".",
-        buttons.BUTTON_QUESTION: "?",
-        buttons.BUTTON_COMMA: ",",
-        buttons.BUTTON_SEMICOLON: ";",
-        buttons.BUTTON_MINUS: "-",
-        buttons.BUTTON_EQUAL: "=",
-        buttons.BUTTON_LEFT_BRACKET: "[",
-        buttons.BUTTON_LEFT_BRACE: "{",
-        buttons.BUTTON_RIGHT_BRACKET: "]",
-        buttons.BUTTON_RIGHT_BRACE: "}",
-        buttons.BUTTON_SLASH: "/",
-        buttons.BUTTON_BACKSLASH: "\\",
-        buttons.BUTTON_UNDERSCORE: "_",
-        buttons.BUTTON_COLON: ":",
-        buttons.BUTTON_SINGLE_QUOTE: "'",
-        buttons.BUTTON_DOUBLE_QUOTE: '"',
-        buttons.BUTTON_PLUS: "+",
-        #
-        buttons.BUTTON_EXCLAMATION: "!",
-        buttons.BUTTON_AT: "@",
-        buttons.BUTTON_HASH: "#",
-        buttons.BUTTON_DOLLAR: "$",
-        buttons.BUTTON_PERCENT: "%",
-        buttons.BUTTON_CARET: "^",
-        buttons.BUTTON_AMPERSAND: "&",
-        buttons.BUTTON_ASTERISK: "*",
-        buttons.BUTTON_LEFT_PARENTHESIS: "(",
-        buttons.BUTTON_RIGHT_PARENTHESIS: ")",
-        buttons.BUTTON_LESS_THAN: "<",
-        buttons.BUTTON_GREATER_THAN: ">",
-        buttons.BUTTON_BACK_TICK: "`",
-        buttons.BUTTON_TILDE: "~",
-        buttons.BUTTON_PIPE: "|",
-    }
-
-    if button in character_map:
-        char_to_add = character_map[button]
-        if shift and char_to_add.isalpha():
-            char_to_add = char_to_add.upper()
-        _textbox.current_text += char_to_add
-        button_pressed = True
-    elif button == buttons.BUTTON_BACKSPACE:
-        _textbox.current_text = _textbox.current_text[:-1]
-        button_pressed = True
-
-    if button_pressed:
-        _textbox.refresh()
 
 
 def start(view_manager) -> bool:
@@ -159,10 +56,7 @@ def run(view_manager) -> None:
         else:
             kb.run()
     else:
-        if not _textbox or but == -1:
-            return
-        inp.reset()
-        __process_text_input(but, inp.was_capitalized)
+        _textbox.run()
 
 
 def stop(view_manager) -> None:

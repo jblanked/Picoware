cf33b39e40b9ee59e9e6b9e3effb9f58b16b41ac
jblanked <82678820+jblanked@users.noreply.github.com>
2026-03-13 20:07:43 -0400

    Update memory.h
    replace new and delete with micropython operators (so gc can manage it)

--- src/MicroPython/engine/memory.h
+++ src/MicroPython/engine/memory.h
@@ -9,4 +9,29 @@ extern "C"
 
 #ifdef __cplusplus
 }
+
+#include <cstddef>
+inline void *operator new(std::size_t size) { return m_malloc(size); }
+inline void *operator new[](std::size_t size) { return m_malloc(size); }
+inline void operator delete(void *p) noexcept
+{
+    if (p)
+        m_free(p);
+}
+inline void operator delete[](void *p) noexcept
+{
+    if (p)
+        m_free(p);
+}
+inline void operator delete(void *p, std::size_t) noexcept
+{
+    if (p)
+        m_free(p);
+}
+inline void operator delete[](void *p, std::size_t) noexcept
+{
+    if (p)
+        m_free(p);
+}
+
 #endif

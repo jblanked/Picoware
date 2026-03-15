c007d171d15c21765d03a115f80700c0a4042ce8
jblanked <82678820+jblanked@users.noreply.github.com>
2026-03-15 16:00:13 -0400

    cleanup vector module and class
    - added c++ guards
    - added setters for x, y, and z
    - in the python class I simplified the imports and added a `__setattr__` so when users set x, y, or z, its set in the C side

--- src/MicroPython/vector/vector_mp.h
+++ src/MicroPython/vector/vector_mp.h
@@ -6,27 +6,39 @@ Source: https://github.com/jblanked/Picoware
 
 #pragma once
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 #include "py/runtime.h"
 #include "py/obj.h"
 #include "py/objarray.h"
 #include "py/mphal.h"
 
-typedef struct
-{
-    mp_obj_base_t base;
-    float x;
-    float y;
-    float z;
-    bool integer;
-} vector_mp_obj_t;
+    typedef struct
+    {
+        mp_obj_base_t base;
+        float x;
+        float y;
+        float z;
+        bool integer;
+    } vector_mp_obj_t;
 
-extern const mp_obj_type_t vector_mp_type;
+    extern const mp_obj_type_t vector_mp_type;
 
-mp_obj_t vector_mp_init(float x, float y, float z, bool integer);
-void vector_mp_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kind_t kind);
-mp_obj_t vector_mp_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args);
-mp_obj_t vector_mp_del(mp_obj_t self_in);
-void vector_mp_attr(mp_obj_t self_in, qstr attribute, mp_obj_t *destination);
-mp_obj_t vector_mp_rotate_y(mp_obj_t self_in, mp_obj_t angle_in);
-mp_obj_t vector_mp_translate(size_t n_args, const mp_obj_t *args);
-mp_obj_t vector_mp_scale(size_t n_args, const mp_obj_t *args);
+    mp_obj_t vector_mp_init(float x, float y, float z, bool integer);
+    void vector_mp_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kind_t kind);
+    mp_obj_t vector_mp_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args);
+    mp_obj_t vector_mp_del(mp_obj_t self_in);
+    void vector_mp_attr(mp_obj_t self_in, qstr attribute, mp_obj_t *destination);
+    mp_obj_t vector_mp_set_x(mp_obj_t self_in, mp_obj_t value_in);
+    mp_obj_t vector_mp_set_y(mp_obj_t self_in, mp_obj_t value_in);
+    mp_obj_t vector_mp_set_z(mp_obj_t self_in, mp_obj_t value_in);
+    mp_obj_t vector_mp_rotate_y(mp_obj_t self_in, mp_obj_t angle_in);
+    mp_obj_t vector_mp_translate(size_t n_args, const mp_obj_t *args);
+    mp_obj_t vector_mp_scale(size_t n_args, const mp_obj_t *args);
+
+#ifdef __cplusplus
+}
+#endif

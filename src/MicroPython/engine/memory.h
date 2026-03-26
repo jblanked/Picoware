4a24d8a1bcb898c9c406e06e88fb6f94cf3b7ecb
jblanked <82678820+jblanked@users.noreply.github.com>
2026-03-25 20:58:55 -0400

    use `m_new` and `m_del`

--- src/MicroPython/engine/memory.h
+++ src/MicroPython/engine/memory.h
@@ -10,28 +10,40 @@ extern "C"
 #ifdef __cplusplus
 }
 
+#include <new>
 #include <cstddef>
-inline void *operator new(std::size_t size) { return m_malloc(size); }
-inline void *operator new[](std::size_t size) { return m_malloc(size); }
+
+// Route global new/delete through MicroPython's allocator
+inline void *operator new(std::size_t size) { return m_new(uint8_t, size); }
+inline void *operator new[](std::size_t size) { return m_new(uint8_t, size); }
 inline void operator delete(void *p) noexcept
 {
     if (p)
-        m_free(p);
+        m_del(uint8_t, (uint8_t *)p, 1);
 }
 inline void operator delete[](void *p) noexcept
 {
     if (p)
-        m_free(p);
+        m_del(uint8_t, (uint8_t *)p, 1);
 }
-inline void operator delete(void *p, std::size_t) noexcept
+inline void operator delete(void *p, std::size_t size) noexcept
 {
     if (p)
-        m_free(p);
+        m_del(uint8_t, (uint8_t *)p, size);
 }
-inline void operator delete[](void *p, std::size_t) noexcept
+inline void operator delete[](void *p, std::size_t size) noexcept
 {
     if (p)
-        m_free(p);
+        m_del(uint8_t, (uint8_t *)p, size);
 }
+
+// Explicit placement-new helpers using m_new/m_del
+#define m_new_class(cls, ...) new (m_new(cls, 1)) cls(__VA_ARGS__)
+#define m_del_class(cls, ptr) \
+    do                        \
+    {                         \
+        (ptr)->~cls();        \
+        m_del(cls, ptr, 1);   \
+    } while (0)
 
 #endif

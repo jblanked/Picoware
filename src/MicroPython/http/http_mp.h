c2b07940059384701e80aa88e6bde7636bf25aa8
jblanked <82678820+jblanked@users.noreply.github.com>
2026-04-19 07:52:32 -0400

    use PicoSDK for http
    switch from using picoware's http class to direct C calls

--- src/MicroPython/http/http_mp.h
+++ src/MicroPython/http/http_mp.h
@@ -6,17 +6,15 @@ Source: https://github.com/jblanked/Picoware
 
 #pragma once
 
+#include <stdbool.h>
+#include <stddef.h>
+
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-#include "py/runtime.h"
-#include "py/obj.h"
-#include "py/objarray.h"
-#include "py/mphal.h"
-
-    bool http_get_http_response(char *buffer, size_t buffer_size);                                         // retrieves the response of the last
+    bool http_get_http_response(char *buffer, size_t buffer_size);                                         // retrieves the response of the last request
     bool http_is_finished(void);                                                                           // returns true if the current request is finished (either successfully or with an error)
     bool http_send_request(const char *url, const char *method, const char *headers, const char *payload); // sends an HTTP request with the given parameters, returns true if the request was successfully initiated
 

c7f0174ec1b9ede26d046bcf74d40a01c65b83a6
jblanked <82678820+jblanked@users.noreply.github.com>
2026-04-28 07:15:31 -0400

    Update config.hpp

--- src/MicroPython/ghouls/Ghouls/src/config.hpp
+++ src/MicroPython/ghouls/Ghouls/src/config.hpp
@@ -37,7 +37,6 @@
 #define HTTP_RECEIVING 1
 #define HTTP_SENDING 2
 #define HTTP_ISSUE 3
-<<<<<<< HEAD
 #define HTTP_INCLUDE "../../http_config.hpp"
 #define HTTP_REQUEST_IS_FINISHED http_is_finished               // () -> bool
 #define HTTP_SEND_REQUEST http_send_request                     // (const char *url, const char *method, const char *headers, const char *payload) -> bool
@@ -47,18 +46,6 @@
 #define HTTP_WEBSOCKET_STOP http_websocket_stop                 // () -> bool
 #define HTTP_GET_RESPONSE http_get_http_response                // (char *buffer, size_t buffer_size) -> bool
 #define HTTP_GET_WEBSOCKET_RESPONSE http_get_websocket_response // (char *buffer, size_t buffer_size) -> bool
-=======
-// #define HTTP_INCLUDE "../http.hpp"
-// #define HTTP_REQUEST_IS_FINISHED http_is_finished               // () -> bool
-// #define HTTP_SEND_REQUEST http_send_request                     // (const char *url, const char *method, const char *headers, const char *payload) -> bool
-// #define HTTP_WEBSOCKET_IS_CONNECTED http_websocket_is_connected // () -> bool
-// #define HTTP_WEBSOCKET_SEND http_websocket_send                 // (const char *message) -> bool
-// #define HTTP_WEBSOCKET_START http_websocket_start               // (const char *url, uint16_t port) -> bool
-// #define HTTP_WEBSOCKET_STOP http_websocket_stop                 // () -> bool
-// #define HTTP_GET_RESPONSE http_get_http_response                // (char *buffer, size_t buffer_size) -> bool
-// #define HTTP_GET_WEBSOCKET_RESPONSE http_get_websocket_response // (char *buffer, size_t buffer_size) -> bool
-// #define HTTP_FILE_DOWNLOAD http_file_download                   // (const char *url, const char *path) -> bool
->>>>>>> e29397b547a00b3ec1d6e34ecf411b4520018728
 
 // json
 #define JSON_INCLUDE "../../../jsmn/jsmn.h"

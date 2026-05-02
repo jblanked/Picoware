cb1c34362ed0e4eaaf45f057d18f2c6cf473fa55
jblanked <82678820+jblanked@users.noreply.github.com>
2026-05-02 09:08:46 -0400

    add storage header/source

--- src/MicroPython/sd/micropython.cmake
+++ src/MicroPython/sd/micropython.cmake
@@ -4,6 +4,7 @@ target_sources(usermod_sd INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/fat32.c
     ${CMAKE_CURRENT_LIST_DIR}/sd_mp.c
     ${CMAKE_CURRENT_LIST_DIR}/sdcard.c
+    ${CMAKE_CURRENT_LIST_DIR}/storage.c
     ${CMAKE_CURRENT_LIST_DIR}/vfs_mp.c
 )
 

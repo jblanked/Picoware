5feca79114625e02e3d99425ce72448f8dc9fe0d
jblanked <82678820+jblanked@users.noreply.github.com>
2026-06-29 08:16:24 -0400

    Squashed 'src/MicroPython/ghouls/Ghouls/' changes from 4435d658..0722d9a4
    0722d9a4 update size
    39ea1994 increase to 256
    b6a92a9b copy full size
    
    git-subtree-dir: src/MicroPython/ghouls/Ghouls
    git-subtree-split: 0722d9a4fbdbada2002290dfe9505c47f5035565

--- src/player.cpp
+++ src/player.cpp
@@ -716,14 +716,14 @@ void Player::drawMapPackView(Draw *canvas)
         mapPackCount = 0;
         for (uint16_t i = 0; i < count && mapPackCount < MAX_MAP_PACK_FILES; i++)
         {
-            snprintf(mapPackFiles[mapPackCount], 64, "%s", rawFiles[i]);
+            snprintf(mapPackFiles[mapPackCount], sizeof(mapPackFiles[mapPackCount]), "%s", rawFiles[i]);
             mapPackCount++;
         }
 #endif
         if (mapPackCount == 0)
         {
             // use default map
-            snprintf(mapPackFiles[0], 64, "%s", "home.ghoulsmap");
+            snprintf(mapPackFiles[0], sizeof(mapPackFiles[0]), "%s", "home.ghoulsmap");
             mapPackCount = 1;
         }
         mapPackSelectedIndex = 0;
@@ -760,8 +760,8 @@ void Player::drawMapPackView(Draw *canvas)
         }
 
         // strip extension
-        char displayName[64];
-        snprintf(displayName, 64, "%s", mapPackFiles[i]);
+        char displayName[256];
+        snprintf(displayName, sizeof(displayName), "%s", mapPackFiles[i]);
         char *dot = strrchr(displayName, '.');
         if (dot)
             *dot = '\0';
@@ -1922,7 +1922,7 @@ void Player::processInput()
             // Store selected map file then load user stats
             if (ghoulsGame && mapPackCount > 0)
             {
-                char fullPath[128];
+                char fullPath[320];
                 snprintf(fullPath, sizeof(fullPath), ASSETS_FOLDER "%s", mapPackFiles[mapPackSelectedIndex]);
                 ghoulsGame->setSelectedMapFile(fullPath);
             }

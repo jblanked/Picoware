ed8c55b036a2b04dcd918f656839a7c9b0e524db
jblanked <82678820+jblanked@users.noreply.github.com>
2026-06-30 16:12:43 -0400

    bump to 1.8.8

--- src/MicroPython/picoware/system/system.py
+++ src/MicroPython/picoware/system/system.py
@@ -170,7 +170,7 @@ class System:
     @property
     def version(self) -> str:
         """Return the Picoware version."""
-        return "1.8.7"
+        return "1.8.8"
 
     def bootloader_mode(self):
         """Enter the bootloader mode."""

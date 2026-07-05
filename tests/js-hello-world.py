80d9c3d4d5bda3d816ceb4edbc56ed7b86d1965d
jblanked <82678820+jblanked@users.noreply.github.com>
2026-07-05 10:34:25 -0400

    simplify example

--- tests/js-hello-world.py
+++ tests/js-hello-world.py
@@ -4,6 +4,8 @@ from picoware.system.js import JS
 vm = ViewManager()
 js = JS(vm)
 
-js.run("draw.clear();")
-js.run("draw.text(10, 10, 'Hello World');")
-js.run("draw.swap();")
+js.run("""
+    draw.clear();
+    draw.text(10, 10, 'Hello World');
+    draw.swap();
+""")

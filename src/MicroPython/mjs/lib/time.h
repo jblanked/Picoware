630cdb20a9a23b4c5024ed73531628ee4ca2cf83
jblanked <82678820+jblanked@users.noreply.github.com>
2026-07-06 07:40:13 -0400

    add prototypes

--- src/MicroPython/mjs/lib/time.h
+++ src/MicroPython/mjs/lib/time.h
@@ -1,4 +1,7 @@
 #pragma once
 #include "../mjs/mjs.h"
 
+void time_js_delay_ms(struct mjs *mjs);
+void time_js_ticks_diff(struct mjs *mjs);
+void time_js_ticks_ms(struct mjs *mjs);
 void time_register(struct mjs *mjs);

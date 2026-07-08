6fcdecfdaf24f441e7c96e7b0fdf6cad568357f4
jblanked <82678820+jblanked@users.noreply.github.com>
2026-07-07 20:41:03 -0400

    add input test

---
+++ tests/js-input.py
@@ -0,0 +1,16 @@
+from picoware.system.js import JS
+
+j = JS()
+
+j.run('let input = import("input");')
+
+print(f"button: {j.run('input.button;')}")
+print(f"battery: {j.run('input.battery;')}")
+print(f"was_capatilized: {j.run('input.wasCapitalized;')}")
+
+#j.run('input.read();')
+print(f"char for button code 5 (BACK): {j.run('input.buttonToChar(5);')}")   # → "" (no mapping)
+print(f"char for button code 38 (5 key): {j.run('input.buttonToChar(38);')}") # → "5"
+print(f"char for button code 7 (A key): {j.run('input.buttonToChar(7);')}")   # → "a"
+print(f"button read: {j.run('input.readNonBlocking();')}")
+j.run('input.reset();')

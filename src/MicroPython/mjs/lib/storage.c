3b7786c727c6c232c33f89e1e0b9d3f7cfa64e96
jblanked <82678820+jblanked@users.noreply.github.com>
2026-07-08 22:41:24 -0400

    check if storage is available

--- src/MicroPython/mjs/lib/storage.c
+++ src/MicroPython/mjs/lib/storage.c
@@ -2,10 +2,20 @@
 #include <string.h>
 #include "py/runtime.h"
 #include "array_buf.h"
+
+#if defined(WAVESHARE_1_28)
+#define STORAGE_NOT_AVAILABLE 1
+#else
 #include "../../sd/storage.h"
+#endif
 
 void storage_read(struct mjs *mjs)
 {
+#if STORAGE_NOT_AVAILABLE
+    mjs_prepend_errorf(mjs, MJS_NOT_IMPLEMENTED_ERROR, "Storage not available on this platform");
+    mjs_return(mjs, MJS_UNDEFINED);
+    return;
+#else
     char *filename_copy = mjs_copy_string_arg(mjs, 0);
     const char *filename = filename_copy ? filename_copy : "";
     const size_t file_size = storage_file_size(filename);
@@ -21,10 +31,16 @@ void storage_read(struct mjs *mjs)
     mjs_return(mjs, mjs_mk_string(mjs, buffer, bytes_read, 1));
     m_free(filename_copy);
     m_free(buffer);
+#endif
 }
 
 void storage_read_chunk(struct mjs *mjs)
 {
+#if STORAGE_NOT_AVAILABLE
+    mjs_prepend_errorf(mjs, MJS_NOT_IMPLEMENTED_ERROR, "Storage not available on this platform");
+    mjs_return(mjs, MJS_UNDEFINED);
+    return;
+#else
     char *filename_copy = mjs_copy_string_arg(mjs, 0);
     const char *filename = filename_copy ? filename_copy : "";
     const size_t offset = (size_t)mjs_get_int(mjs, mjs_arg(mjs, 1));
@@ -41,19 +57,31 @@ void storage_read_chunk(struct mjs *mjs)
     mjs_return(mjs, mjs_mk_string(mjs, buffer + offset, bytes_read, 1));
     m_free(filename_copy);
     m_free(buffer);
+#endif
 }
 
 void storage_size(struct mjs *mjs)
 {
+#if STORAGE_NOT_AVAILABLE
+    mjs_prepend_errorf(mjs, MJS_NOT_IMPLEMENTED_ERROR, "Storage not available on this platform");
+    mjs_return(mjs, MJS_UNDEFINED);
+    return;
+#else
     char *filename_copy = mjs_copy_string_arg(mjs, 0);
     const char *filename = filename_copy ? filename_copy : "";
     const size_t file_size = storage_file_size(filename);
     mjs_return(mjs, mjs_mk_number(mjs, file_size));
     m_free(filename_copy);
+#endif
 }
 
 void storage_write(struct mjs *mjs)
 {
+#if STORAGE_NOT_AVAILABLE
+    mjs_prepend_errorf(mjs, MJS_NOT_IMPLEMENTED_ERROR, "Storage not available on this platform");
+    mjs_return(mjs, MJS_UNDEFINED);
+    return;
+#else
     char *filename_copy = mjs_copy_string_arg(mjs, 0);
     const char *filename = filename_copy ? filename_copy : "";
     mjs_val_t data = mjs_arg(mjs, 1);
@@ -76,10 +104,16 @@ void storage_write(struct mjs *mjs)
     }
     mjs_return(mjs, mjs_mk_boolean(mjs, storage_file_write(filename, buf, len)));
     m_free(filename_copy);
+#endif
 }
 
 void storage_create(struct mjs *mjs, mjs_val_t *storage_obj)
 {
+#if STORAGE_NOT_AVAILABLE
+    mjs_prepend_errorf(mjs, MJS_NOT_IMPLEMENTED_ERROR, "Storage not available on this platform");
+    mjs_return(mjs, MJS_UNDEFINED);
+    return;
+#else
     *storage_obj = mjs_mk_object(mjs);
 
     mjs_set(mjs, *storage_obj, "read", ~0,
@@ -90,4 +124,5 @@ void storage_create(struct mjs *mjs, mjs_val_t *storage_obj)
             mjs_mk_foreign_func(mjs, (mjs_func_ptr_t)storage_size));
     mjs_set(mjs, *storage_obj, "write", ~0,
             mjs_mk_foreign_func(mjs, (mjs_func_ptr_t)storage_write));
+#endif
 }

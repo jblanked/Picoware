916cf2f5b6471eef293dbb6cf62b60af78156c0a
jblanked <82678820+jblanked@users.noreply.github.com>
2026-07-21 11:57:58 -0400

    build `usb_video` module in pancake

--- src/MicroPython/Pancake/micropython.cmake
+++ src/MicroPython/Pancake/micropython.cmake
@@ -31,6 +31,7 @@ include(${CMAKE_CURRENT_LIST_DIR}/../mjs/micropython.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/../picoware_boards/micropython.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/../response/micropython.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/../textbox/micropython.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../usb_video/micropython.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/../vector/micropython.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/../vt/micropython.cmake)
 

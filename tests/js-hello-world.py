27424485871dd49760b278c64eabc7b951f66cb2
jblanked <82678820+jblanked@users.noreply.github.com>
2026-08-02 17:36:26 -0400

    update `draw` js module with `len` + `screenshot`

--- tests/js-hello-world.py
+++ tests/js-hello-world.py
@@ -2,9 +2,13 @@ from picoware.system.js import JS
 
 js = JS()
 
-js.run("""
+res = js.run("""
     let draw = import('draw');
     draw.clear();
     draw.text(10, 10, 'Hello World');
     draw.swap();
+    draw.screenshot("js.bmp")
+    let pixels = draw.len("Hello World");
 """)
+
+print(f"Hello world is {res} pixels long")

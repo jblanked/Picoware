b805e6523eb7576dd303c04c2dcf92583f4fad62
jblanked <82678820+jblanked@users.noreply.github.com>
2026-08-02 20:19:47 -0400

    update agent test

--- tests/agent.py
+++ tests/agent.py
@@ -1,4 +1,4 @@
-from picoware.system.agent.agent import Agent
+from picoware.system.agent.agent import Agent, MODE_DEVICE_MANAGER, MODE_APP_CREATOR
 from picoware.system.agent.llm import DEEPSEEK, LLM
 from picoware.system.view_manager import ViewManager
 
@@ -6,7 +6,7 @@ from picoware.system.view_manager import ViewManager
 vm = ViewManager()
 
 # simple
-agent = Agent(vm)
+agent = Agent(vm, MODE_DEVICE_MANAGER)
 topic = "What can you tell me about my device?"
 response = agent.run(topic)
 print("Agent response:", response)
@@ -16,7 +16,7 @@ del response
 
 # advanced
 llm = LLM(vm.storage, DEEPSEEK, "deepseek-v4-flash")
-agent = Agent(vm, llm)
+agent = Agent(vm, MODE_APP_CREATOR, llm)
 topic = "Make me a picoware app that says 'Hello Picoware' and changes color when I click the center button"
 response = agent.run(topic)
 print("Agent response:", response)

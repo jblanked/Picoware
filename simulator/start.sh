bd6a7293d4e2e7e22821aae84dcc13e0690fd3ea
Slasher006 <martinwietzke72@gmail.com>
2026-08-04 05:55:00 +0200

    Remove simulator viewer launcher

--- simulator/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -eu
-
-script_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
-repo_root=$(dirname -- "$script_dir")
-
-cd "$repo_root"
-exec micropython simulator/run.py --viewer "$@"

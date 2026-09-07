ad197a9980622cf34e0216d3fc132297c42ee5a3
jblanked <82678820+jblanked@users.noreply.github.com>
2026-09-07 13:52:24 -0400

    cleanup espflasher

--- tests/espflasher.py
+++ tests/espflasher.py
@@ -1,29 +1,54 @@
-from picoware.system.espflasher import ESPFlasher
+from picoware.system.drivers.espflasher import ESPFlasher
 from machine import Pin
 from picoware.system.uart import UART
+from picoware.system.view_manager import ViewManager
+from gc import mem_free, collect
+from flipper_battery import init, shutdown
 
-if __name__ == "__main__":
-    reset = Pin(3, Pin.OUT)
-    gpio0 = Pin(2, Pin.OUT)
-    uart = UART(1, 8, 9, 115200, 350)
+def clean(num):
+    collect()
+    print(f"mem_free({mem_free()})-{num}")
 
-    md5sum = b"9a6cf1257769c9f1af08452558e4d60e"
-    path = "NINA_W102-v1.5.0-Nano-RP2040-Connect.bin"
+def main():
+    # devboard docs: https://developer.flipper.net/flipperzero/doxygen/dev_board.html
+    clean(1)
+    #init()
+    #vm = ViewManager()
+    clean(2)
+    uart = UART()
+    reset = Pin(Pin.cpu.B2, Pin.OUT) # on devboard, its the 6th pin from the left (RTS/EN)
+    gpio0 = Pin(Pin.cpu.C3, Pin.OUT) # on devboard, its the 7th pin from the left (DTR/BOOT)
+    clean(3)
 
-    esp = ESPFlasher(reset, gpio0, uart)
+    path = "/sd/flipper_http_merged.bin"
+
+    esp = ESPFlasher(reset, gpio0, uart, chip="esp32s2")
+    clean(4)
     # Enter bootloader download mode, at 115200
-    esp.bootloader()
+    attempts = 10
+    esp.bootloader(attempts)
+    clean(5)
+
     # Can now change to higher/lower baudrate
-    esp.set_baudrate(921600)
+    esp.set_baudrate(460800)
     # Must call this first before any flash functions.
     esp.flash_attach()
+    clean(6)
     # Read flash size
     size = esp.flash_read_size()
     # Configure flash parameters.
     esp.flash_config(size)
+    clean(7)
     # Write firmware image from internal storage.
-    esp.flash_write_file(path)
-    # Compares file and flash MD5 checksum.
-    esp.flash_verify_file(path, md5sum)
+    esp.flash_write_file(path, blksize=0x400, progress_interval=128)
+    clean(8)
     # Resets the ESP32 chip.
     esp.reboot()
+    clean(9)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        print(e)
+    clean(11)

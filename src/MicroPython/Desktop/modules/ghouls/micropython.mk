27a9e47bf685d64bd43dbf00f9ce9b505ac92279
Slasher006 <martinwietzke72@gmail.com>
2026-09-12 15:29:18 +0200

    Refine PR 292 review fixes

--- src/MicroPython/Desktop/modules/ghouls/micropython.mk
+++ src/MicroPython/Desktop/modules/ghouls/micropython.mk
@@ -1,18 +1,20 @@
-DESKTOP_GHOULS_DIR := $(USERMOD_DIR)/../../../ghouls
-DESKTOP_GHOULS_MOD_DIR := $(USERMOD_DIR)
-DESKTOP_GHOULS_SOURCES := $(wildcard $(DESKTOP_GHOULS_DIR)/Ghouls/src/*.cpp)
+PICOWARE_MICROPYTHON_DIR := $(USERMOD_DIR)/../../..
+GHOULS_DIR := $(PICOWARE_MICROPYTHON_DIR)/ghouls
+GHOULS_MOD_DIR := $(USERMOD_DIR)
+JSMN_DIR := $(PICOWARE_MICROPYTHON_DIR)/jsmn
+GHOULS_SOURCES := $(wildcard $(GHOULS_DIR)/Ghouls/src/*.cpp)
 
 # Use the firmware game and the engine already linked by the Desktop port.
-SRC_USERMOD_CXX += $(DESKTOP_GHOULS_MOD_DIR)/binding.cpp
-SRC_USERMOD_LIB_CXX += $(DESKTOP_GHOULS_SOURCES)
-SRC_USERMOD += $(DESKTOP_GHOULS_MOD_DIR)/bridge.c
-SRC_USERMOD += $(USERMOD_DIR)/../../../jsmn/jsmn.c
-SRC_USERMOD += $(USERMOD_DIR)/../../../jsmn/jsmn_h.c
-CXXFLAGS_USERMOD += -I$(DESKTOP_GHOULS_DIR)
-CXXFLAGS_USERMOD += -I$(DESKTOP_GHOULS_DIR)/Ghouls/src
-CXXFLAGS_USERMOD += -I$(DESKTOP_GHOULS_DIR)/Ghouls/src/pico-game-engine
-CXXFLAGS_USERMOD += -I$(DESKTOP_GHOULS_MOD_DIR)
+SRC_USERMOD_CXX += $(GHOULS_MOD_DIR)/binding.cpp
+SRC_USERMOD_LIB_CXX += $(GHOULS_SOURCES)
+SRC_USERMOD += $(GHOULS_MOD_DIR)/bridge.c
+SRC_USERMOD += $(JSMN_DIR)/jsmn.c
+SRC_USERMOD += $(JSMN_DIR)/jsmn_h.c
+CXXFLAGS_USERMOD += -I$(GHOULS_DIR)
+CXXFLAGS_USERMOD += -I$(GHOULS_DIR)/Ghouls/src
+CXXFLAGS_USERMOD += -I$(GHOULS_DIR)/Ghouls/src/pico-game-engine
+CXXFLAGS_USERMOD += -I$(GHOULS_MOD_DIR)
 
 # Only game translation units need the Desktop configuration adapter.
-DESKTOP_GHOULS_OBJECTS := $(addprefix $(BUILD)/,$(patsubst $(USER_C_MODULES)/%.cpp,%.o,$(DESKTOP_GHOULS_SOURCES)))
-$(DESKTOP_GHOULS_OBJECTS): CXXFLAGS += -include $(DESKTOP_GHOULS_MOD_DIR)/compat.hpp
+GHOULS_OBJECTS := $(addprefix $(BUILD)/,$(patsubst $(USER_C_MODULES)/%.cpp,%.o,$(GHOULS_SOURCES)))
+$(GHOULS_OBJECTS): CXXFLAGS += -include $(GHOULS_MOD_DIR)/compat.hpp
--- src/MicroPython/ghouls/Ghouls/src/engine_config.hpp
+++ src/MicroPython/ghouls/Ghouls/src/engine_config.hpp
@@ -8,14 +8,6 @@ extern "C"
 }
 #define TIME_INCLUDE "py/mphal.h"
 #define TIME_MILLIS mp_hal_ticks_ms()
-#elif defined(CARDPUTER) || defined(ESP32) || defined(CROWPANEL_10_1) || defined(WAVESHARE_2_06) || defined(PANCAKE) || defined(V8)
-#include "esp_timer.h"
-#define TIME_INCLUDE "esp_timer.h"
-#define TIME_MILLIS esp_timer_get_time() / 1000
-#else
-#include "pico/time.h"
-#define TIME_INCLUDE "pico/time.h"
-#define TIME_MILLIS to_ms_since_boot(get_absolute_time())
 #endif
 
 // general

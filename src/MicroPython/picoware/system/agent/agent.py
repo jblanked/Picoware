d94efabbeb175cd286492f9ceb30e5cb5640fc68
jblanked <82678820+jblanked@users.noreply.github.com>
2026-09-21 09:38:05 -0400

    check mode before downloading

--- src/MicroPython/picoware/system/agent/agent.py
+++ src/MicroPython/picoware/system/agent/agent.py
@@ -582,28 +582,29 @@ class Agent:
         if context is not None:
             s.write(self._mem_path, f"{context.strip()}\n", mode="a")
         else:
-            if not s.exists("picoware/assets/agents/app_creator_context.md"):
-                if not s.mkdir("picoware/assets/agents"):
-                    return "An error occurred during processing: Failed to create directory for agent context."
-                self.view_manager.log("Fetching app creator context...")
-                # https://raw.githubusercontent.com/{_github_author}/{_github_repo}/HEAD/{path}
-                if not self.http.request_async("GET", "https://raw.githubusercontent.com/jblanked/Picoware/dev/builds/MicroPython/assets/agent/app_creator_context.md", save_to_file="picoware/assets/agents/app_creator_context.md", storage=s, headers={
-                                            "User-Agent": "Raspberry Pi Pico W",
-                                            "Content-Type": "application/octet-stream",
-                                        }
-                            ):
-                    return "An error occurred during processing: No agent context found and failed to fetch app creator context."
-                inp = self.view_manager.input_manager
-                inp.reset()
-                while self.http.in_progress:
-                    but = inp.button
-                    if but != -1:
-                        inp.reset()
-                        self.http.close()
-                        break
-                if not self.http.is_successful or not s.exists("picoware/assets/agents/app_creator_context.md"):
-                    return "An error occurred during processing: No agent context found and failed to fetch app creator context."
-                self.view_manager.log("App creator context fetched successfully.")
+            if self.mode == MODE_APP_CREATOR:
+                if not s.exists("picoware/assets/agents/app_creator_context.md"):
+                    if not s.mkdir("picoware/assets/agents"):
+                        return "An error occurred during processing: Failed to create directory for agent context."
+                    self.view_manager.log("Fetching app creator context...")
+                    # https://raw.githubusercontent.com/{_github_author}/{_github_repo}/HEAD/{path}
+                    if not self.http.request_async("GET", "https://raw.githubusercontent.com/jblanked/Picoware/dev/builds/MicroPython/assets/agent/app_creator_context.md", save_to_file="picoware/assets/agents/app_creator_context.md", storage=s, headers={
+                                                "User-Agent": "Raspberry Pi Pico W",
+                                                "Content-Type": "application/octet-stream",
+                                            }
+                                ):
+                        return "An error occurred during processing: No agent context found and failed to fetch app creator context."
+                    inp = self.view_manager.input_manager
+                    inp.reset()
+                    while self.http.in_progress:
+                        but = inp.button
+                        if but != -1:
+                            inp.reset()
+                            self.http.close()
+                            break
+                    if not self.http.is_successful or not s.exists("picoware/assets/agents/app_creator_context.md"):
+                        return "An error occurred during processing: No agent context found and failed to fetch app creator context."
+                    self.view_manager.log("App creator context fetched successfully.")
             f = s.file_open(self._mem_path)
             if f is not None:
                 try:
